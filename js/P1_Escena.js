ed3e797eb3e8e26b2161dcf98c24c87d2c4e2b88
laiiabr25 <132926655+laiiabr25@users.noreply.github.com>
2025-02-09 23:06:09 +0100

    p1

--- js/P1_Escena.js
+++ js/P1_Escena.js
@@ -28,39 +28,41 @@ function init()
 {
     // Motor de render
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    //renderer.setClearColor( new THREE.Color(0x0000AA) );
-    document.getElementById('container').appendChild( renderer.domElement );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    //renderer.setClearColor(new THREE.Color(0x0000AA));
+    document.getElementById('container').appendChild(renderer.domElement);
 
     // Escena
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0.5,0.5,0.5);
 
     // Camara
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,1000);
-    camera.position.set( 0.5, 2, 7 );
-    camera.lookAt( new THREE.Vector3(0,1,0) );
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    camera.position.set(0.5, 2, 7);
+    camera.lookAt(new THREE.Vector3(0, 1, 0));
+
+    // Ajustar tamaño de ventana
+    window.addEventlistener('resize', updateAspectRatio);
 }
 
 function loadScene()
 {
-    const material = new THREE.MeshBasicMaterial( { color: 'yellow', wireframe: true } );
+    const material = new THREE.MeshBasicMaterial({ color: 'yellow', wireframe: true });
 
-    const geoCubo = new THREE.BoxGeometry( 2,2,2 );
-    const geoEsfera = new THREE.SphereGeometry( 1, 20,20 );
+    const geoCubo = new THREE.BoxGeometry(2, 2, 2);
+    const geoEsfera = new THREE.SphereGeometry(1, 20, 2 );
 
-    const cubo = new THREE.Mesh( geoCubo, material );
-    const esfera = new THREE.Mesh( geoEsfera, material );
+    const cubo = new THREE.Mesh(geoCubo, material);
+    const esfera = new THREE.Mesh(geoEsfera, material);
 
     // Suelo
-    const suelo = new THREE.Mesh( new THREE.PlaneGeometry(10,10, 10,10), material );
+    const suelo = new THREE.Mesh(new THREE.PlaneGeometry(10,10, 10,10), material);
     suelo.rotation.x = -Math.PI / 2;
     scene.add(suelo);
 
     // Importar un modelo en json
     const loader = new THREE.ObjectLoader();
-
-    loader.load( 'models/soldado/soldado.json', 
+    loader.load('models/soldado/soldado.json', 
         function(objeto){
             cubo.add(objeto);
             objeto.position.y = 1;
@@ -71,14 +73,13 @@ function loadScene()
     esferaCubo.position.y = 1.5;
     cubo.position.x = -1;
     esfera.position.x = 1;
-    cubo.add( new THREE.AxesHelper(1) );
+    cubo.add(new THREE.AxesHelper(1));
 
-    scene.add( esferaCubo);
-    esferaCubo.add( cubo );
-    esferaCubo.add( esfera );
+    scene.add(esferaCubo);
+    esferaCubo.add(cubo);
+    esferaCubo.add(esfera);
  
-
-    scene.add( new THREE.AxesHelper(3) );
+    scene.add(new THREE.AxesHelper(3));
 
 }
 
@@ -90,7 +91,7 @@ function update()
 
 function render()
 {
-    requestAnimationFrame( render );
+    requestAnimationFrame(render);
     update();
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
