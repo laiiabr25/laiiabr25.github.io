36899b1aa800e107a4478d33dbb7157b71436ae5
Laia Benavent Ribelles <LBENRIB@upv.edu.es>
2025-02-12 16:53:28 +0100

    Práctica 2

--- js/P2_EscenaAnimada.js
+++ js/P2_EscenaAnimada.js
@@ -16,8 +16,6 @@
 import * as THREE from "../lib/three.module.js"
 import {GLTFLoader} from "../lib/GLTFLoader.module.js"
 import {OrbitControls} from "../lib/OrbitControls.module.js"
-import {TWEEN} from "../lib/tween.module.min.js"
-import {GUI} from "../lib/lil-gui.module.min.js"
 
 // Variables de consenso
 let renderer, scene, camera;
@@ -26,8 +24,8 @@ let renderer, scene, camera;
 /*******************
  * TO DO: Variables globales de la aplicacion
  *******************/
-let cameraControls, effectController;
-let pentagono, cubo, esfera, cilindro, cono;
+let cameraControls;
+let pentagono;
 let angulo = 0;
 
 // Acciones
@@ -44,24 +42,26 @@ function init()
     /*******************
     * TO DO: Completar el motor de render y el canvas
     *******************/
-    document.getElementById('container').appendChild(render.domElement);
+    document.getElementById('container').appendChild(renderer.domElement);
 
     // Escena
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0.5, 0.5, 0.5);
-    
+
     // Camara
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-    camera.position.set(0.5, 2, 7);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0.5, 2, 10);
     /*******************
     * TO DO: Añadir manejador de camara (OrbitControls)
     *******************/
+    cameraControls = new OrbitControls(camera, renderer.domElement);
+    cameraControls.target.set(0, 1, 0);
     camera.lookAt(new THREE.Vector3(0, 1, 0));
 }
 
 function loadScene()
 {
-    const material = new THREE.MeshNormalMaterial( {wireframe:false} );
+    const material = new THREE.MeshBasicMaterial({ color: 'yellow', wireframe: true });
 
     /*******************
     * TO DO: Misma escena que en la practica anterior
@@ -77,10 +77,10 @@ function loadScene()
     const geoCilindro = new THREE.CylinderGeometry(1, 1, 2, 20)
     const geoCono = new THREE.ConeGeometry(1, 2, 20);
 
-    cubo = new THREE.Mesh(geoCubo, material);
-    esfera = new THREE.Mesh(geoEsfera, material);
-    cilindro = new THREE.Mesh(geoCilindro, material);
-    cono = new THREE.Mesh(geoCono, material);
+    const cubo = new THREE.Mesh(geoCubo, material);
+    const esfera = new THREE.Mesh(geoEsfera, material);
+    const cilindro = new THREE.Mesh(geoCilindro, material);
+    const cono = new THREE.Mesh(geoCono, material);
 
     pentagono = new THREE.Object3D();
     pentagono.position.y = 1.5;
@@ -137,7 +137,7 @@ function loadGUI()
     *******************/
 }
 
-function update(delta)
+function update()
 {
     /*******************
     * TO DO: Actualizar tween
@@ -146,9 +146,9 @@ function update(delta)
     pentagono.rotation.y = angulo;
 }
 
-function render(delta)
+function render()
 {
     requestAnimationFrame(render);
-    update(delta);
+    update();
     renderer.render(scene, camera);
 }
