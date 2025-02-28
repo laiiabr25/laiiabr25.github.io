d63b27e28c882a47bef2ed70dd60433de331a2c8
laiiabr25 <laibenrib25@gmail.com>
2025-02-28 22:45:32 +0100

    Trabajo - Fase 1 (escalar y centrar clave de sol)

--- js/Trabajo.js
+++ js/Trabajo.js
@@ -77,11 +77,11 @@ function cargarClaveSol() {
         // Calcular los límites del modelo
         const box = new THREE.Box3().setFromObject(claveSol);
         const center = box.getCenter(new THREE.Vector3());
-        const size = box.getSize(new THREE.Vector3());
         // Centrar el modelo en el origen de coordenadas
-        claveSol.position.sub(center);
+        //claveSol.position.sub(center);
         // Ajustar la posición en Y para que toque el suelo
-        claveSol.position.y = -size.y / 2;
+        claveSol.position.y = 1;
+        claveSol.rotation.y = -Math.PI / 2;
         // Escalar si es necesario
         claveSol.scale.set(0.5, 0.5, 0.5);
         scene.add(claveSol);
