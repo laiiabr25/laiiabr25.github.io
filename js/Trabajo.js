9c96f8abae70c8e723db60c1128a055c64ec5547
laiiabr25 <laibenrib25@gmail.com>
2025-03-01 18:44:10 +0100

    Trabajo - Fase 1 (escalar trompa)

--- js/Trabajo.js
+++ js/Trabajo.js
@@ -120,9 +120,12 @@ function cargarInstrumento(nombre) {
             instrumentoActual.scale.set(3 / size.y, 3 / size.y, 3 / size.y);
         }
         else if (nombre === "trombon") {
-            instrumentoActual.rotation.x = Math.PI / 2;
+            instrumentoActual.rotation.x = -Math.PI / 2;
             instrumentoActual.rotation.y = Math.PI / 2;
             instrumentoActual.scale.set(0.7 / size.y, 0.7 / size.y, 0.7 / size.y);
+        }
+        else if (nombre === "trompa") {
+            instrumentoActual.scale.set(3 / size.y, 3 / size.y, 3 / size.y);
         }
         else {
             instrumentoActual.scale.set(4 / size.y, 4 / size.y, 4 / size.y);
