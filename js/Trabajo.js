44dcf3ff85771c5c267a541882ee9956d5e4b1a8
laiiabr25 <laibenrib25@gmail.com>
2025-06-04 14:42:42 +0200

    Caché glTF

--- js/Trabajo.js
+++ js/Trabajo.js
@@ -8,6 +8,7 @@ let instrumentoSeleccionado = null;
 let listaInstrumentos = [ "bateria", "clarinete", "flauta", "guitarra_acustica", "marimba", "piano", "saxo", "trombon", "trompa", "trompeta", "violin" ];
 let rotadorInstrumento = new THREE.Object3D();
 let sonidoActual = null;
+let modelosCache = {};
 
 init();
 loadScene();
@@ -52,7 +53,7 @@ function loadScene() {
     const textura = new THREE.TextureLoader().load("images/wood512.jpg");
     const material = new THREE.MeshStandardMaterial({ map: textura });
 
-    const luzAmbiente = new THREE.AmbientLight(0xffffff, 1.2);
+    const luzAmbiente = new THREE.AmbientLight(0x404040, 0.8);
     scene.add(luzAmbiente);
 
     const luzDireccional = new THREE.DirectionalLight(0xffffff, 0.6);
@@ -67,7 +68,7 @@ function loadScene() {
     tarima.receiveShadow = true;
     scene.add(tarima);
 
-    const pared = new THREE.Mesh(new THREE.PlaneGeometry(10, 6), material);
+    const pared = new THREE.Mesh(new THREE.PlaneGeometry(10, 8), material);
     pared.position.set(0, 2.5, -5);
     scene.add(pared);
 
@@ -89,24 +90,28 @@ function cargarInstrumento(nombre) {
         return;
     }
 
+    if (modelosCache[nombre]) {
+        usarInstrumento(modelosCache[nombre], nombre);
+        return;
+    }
+
     const rutaBase = `models/instrumentos/${nombre}/`;
     const loader = new GLTFLoader();
     loader.setPath(rutaBase);
     loader.setResourcePath(rutaBase);
 
-    const loadingMessage = document.createElement("div");
-    loadingMessage.textContent = `Cargando... ${nombre}`;
-    loadingMessage.style.position = "absolute";
-    loadingMessage.style.top = "20%";
-    loadingMessage.style.left = "50%";
-    loadingMessage.style.transform = "translate(-50%, -50%)";
-    loadingMessage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-    loadingMessage.style.color = "white";
-    loadingMessage.style.padding = "10px";
-    loadingMessage.style.borderRadius = "5px";
-    document.body.appendChild(loadingMessage);
+    const loadingMessage = crearMensajeCarga(nombre);
 
     loader.load("scene.gltf", function(gltf) {
+        modelosCache[nombre] = gltf.scene.clone(true);
+        usarInstrumento(modelosCache[nombre], nombre);
+        document.body.removeChild(loadingMessage);
+    }, undefined, function (error) {
+        console.error("Error al cargar:", error);
+        document.body.removeChild(loadingMessage);
+    });
+
+    /*loader.load("scene.gltf", function(gltf) {
         if (instrumentoActual) {
             instrumentoActual.traverse(child => {
                 if (child.isMesh) {
@@ -128,9 +133,6 @@ function cargarInstrumento(nombre) {
                 if (nombre === "clave") {
                     node.material = new THREE.MeshStandardMaterial({color: "black", wireframe: true, side: THREE.DoubleSide});
                 } else {
-                    /*if (!node.material.map) {
-                        node.material = new THREE.MeshStandardMaterial({color: 0xaaaaaa, metalness: 0.4, roughness: 0.5});
-                    }*/
                     node.material.envMap = scene.environment;
                     node.material.envMapIntensity = 1;
                     node.material.needsUpdate = true;
@@ -152,7 +154,66 @@ function cargarInstrumento(nombre) {
     }, undefined, function(error) {
         console.error("Error al cargar el modelo:", error);
         document.body.removeChild(loadingMessage);
+    });*/
+}
+
+function crearMensajeCarga(nombre) {
+    const loadingMessage = document.createElement("div");
+    loadingMessage.textContent = `Cargando... ${nombre}`;
+    loadingMessage.style.position = "absolute";
+    loadingMessage.style.top = "20%";
+    loadingMessage.style.left = "50%";
+    loadingMessage.style.transform = "translate(-50%, -50%)";
+    loadingMessage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    loadingMessage.style.color = "white";
+    loadingMessage.style.padding = "10px";
+    loadingMessage.style.borderRadius = "5px";
+
+    document.body.appendChild(loadingMessage);
+}
+
+function usarInstrumento(objeto, nombre) {
+    if (instrumentoActual) {
+        instrumentoActual.traverse(child => {
+            if (child.isMesh) {
+                child.geometry.dispose();
+                if (Array.isArray(child.material)) {
+                    child.material.forEach(m => m.dispose());
+                } else {
+                    child.material.dispose();
+                }
+            }
+        });
+        rotadorInstrumento.remove(instrumentoActual);
+    }
+    
+    instrumentoActual = objeto.clone(true);
+    instrumentoActual.traverse(node => {
+        if (node.isMesh) {
+            node.castShadow = true;
+            node.receiveShadow = true;
+            if (nombre === "clave") {
+                node.material = new THREE.MeshStandardMaterial({color: "black", wireframe: true, side: THREE.DoubleSide});
+            } else {
+                node.material.envMap = scene.environment;
+                node.material.envMapIntensity = 1;
+                node.material.needsUpdate = true;
+            }
+        }
     });
+
+    ajustarInstrumento(instrumentoActual, nombre);
+    rotadorInstrumento.add(instrumentoActual);
+
+    if (sonidoActual) {
+        sonidoActual.pause();
+        sonidoActual.currentTime = 0;
+    }
+    if (nombre != "clave") {
+        sonidoActual = new Audio(`sounds/${nombre}.mp3`);
+        sonidoActual.play();
+    }
+    instrumentoSeleccionado = nombre;
 }
 
 function ajustarInstrumento(objeto, nombre) {
@@ -186,46 +247,6 @@ function ajustarInstrumento(objeto, nombre) {
     } else if (nombre === "violin") {
         instrumentoActual.rotation.set(Math.PI / 2, 0, 0);
     }
-
-    /*if (nombre === "clave") {
-        instrumentoActual.scale.set(5 / size.y, 5 / size.y, 5 / size.y);
-    }
-    else if (nombre === "bateria") {
-        instrumentoActual.rotation.y = Math.PI;
-        instrumentoActual.scale.set(3.5 / size.y, 3.5 / size.y, 3.5 / size.y)
-    }
-    else if (nombre === "clarinete") {
-        instrumentoActual.rotation.set(0, 0, 0);
-        instrumentoActual.rotation.x = Math.PI / 2;
-        instrumentoActual.rotation.y = Math.PI;
-        instrumentoActual.scale.set(0.5 / size.y, 0.5 / size.y, 0.5 / size.y);
-    }
-    else if (nombre === "flauta") {
-        instrumentoActual.rotation.set(0, 0, 0);
-        instrumentoActual.rotation.x = Math.PI / 2;
-        instrumentoActual.scale.set(0.2 / size.y, 0.2 / size.y, 0.2 / size.y);
-    }
-    else if (nombre === "marimba") {
-        instrumentoActual.scale.set(2.5 / size.y, 2.5 / size.y, 2.5 / size.y);
-    }
-    else if (nombre === "trombon") {
-        instrumentoActual.rotation.x = Math.PI / 2;
-        instrumentoActual.rotation.y = Math.PI / 2;
-        instrumentoActual.scale.set(0.7 / size.y, 0.7 / size.y, 0.7 / size.y);
-    }
-    else if (nombre === "trompa") {
-        instrumentoActual.scale.set(2.5 / size.y, 2.5 / size.y, 2.5 / size.y);
-    }
-    else if (nombre === "trompeta") {
-        instrumentoActual.scale.set(1.5 / size.y, 1.5 / size.y, 1.5 / size.y);
-    }
-    else if (nombre === "violin") {
-        instrumentoActual.rotation.set(Math.PI / 2, 0, 0);
-        instrumentoActual.scale.set(0.5 / size.y, 0.5 / size.y, 0.5 / size.y);
-    }
-    else {
-        instrumentoActual.scale.set(4 / size.y, 4 / size.y, 4 / size.y);
-    }*/
 
     const boxScaled = new THREE.Box3().setFromObject(instrumentoActual);
     const center = boxScaled.getCenter(new THREE.Vector3());
